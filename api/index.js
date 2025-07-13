37d60831a096563aaca4f0f1797a2e8149e5d628
gordonmaloney <gordonmaloney@gmail.com>
2025-07-13 08:56:45 +0100

    testing rate limiting

--- api/index.js
+++ api/index.js
@@ -16,8 +16,8 @@ const app = express();
 
 // apply rate-limit to /campaigns
 const campaignsLimiter = rateLimit({
-  windowMs: 15 * 60 * 1000,   // 15 minutes
-  max: 5,                   // limit each IP to 100 requests per window
+  windowMs: 5 * 60 * 1000,   // 15 minutes
+  max: 100,                   // limit each IP to 100 requests per window
   standardHeaders: true,      // Return rate limit info in the `RateLimit-*` headers
   legacyHeaders: false,       // Disable the `X-RateLimit-*` headers
   message: {
