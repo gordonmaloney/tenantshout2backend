274b63104e56e46e7f5fb33f90aea0e0f623c44f
gordonmaloney <gordonmaloney@gmail.com>
2025-11-02 08:19:00 +0000

    subject string or array

--- models/Campaign.js
+++ models/Campaign.js
@@ -1,38 +1,41 @@
 const mongoose = require("mongoose");
 
-const CampaignSchema = new mongoose.Schema({
-  campaignId: { type: String, required: true, unique: true },
-  campaign: {
-    id: String,
-    title: String,
-    host: String,
-    channel: String,
-    target: String,
-    link: String,
-    blurb: String,
-    subject: String,
-    bcc: String,
-    customTarget: mongoose.Schema.Types.Mixed,
-    customTargetting: String,
-    prompts: [
-      {
-        id: String,
-        question: String,
-        answerType: String,
-        required: Boolean,
-        answer: mongoose.Schema.Types.Mixed,
-      },
-    ],
-    template: String,
-    accordion: [
-      {
-        q: String,
-        a: mongoose.Schema.Types.Mixed,
-      },
-    ],
+const CampaignSchema = new mongoose.Schema(
+  {
+    campaignId: { type: String, required: true, unique: true },
+    campaign: {
+      id: String,
+      title: String,
+      host: String,
+      channel: String,
+      target: String,
+      link: String,
+      blurb: String,
+      subject: mongoose.Schema.Types.Mixed,
+      bcc: String,
+      customTarget: mongoose.Schema.Types.Mixed,
+      customTargetting: String,
+      prompts: [
+        {
+          id: String,
+          question: String,
+          answerType: String,
+          required: Boolean,
+          answer: mongoose.Schema.Types.Mixed,
+        },
+      ],
+      template: String,
+      accordion: [
+        {
+          q: String,
+          a: mongoose.Schema.Types.Mixed,
+        },
+      ],
+    },
   },
-},   {
-  timestamps: true, // ← adds createdAt and updatedAt
-});
+  {
+    timestamps: true, // ← adds createdAt and updatedAt
+  }
+);
 
 module.exports = mongoose.model("Campaign", CampaignSchema);
